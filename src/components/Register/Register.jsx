750376d1dfa3ed0e6e27400f5a626b3adbde2791
Sumaiyakhan322 <sumaiya.khan32665@gmail.com>
2023-10-02 01:46:01 +0600

    added show password , regular expression for password

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,12 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import auth from "../../firebase_config";
-
+import { FaEye,FaEyeSlash } from 'react-icons/fa';
 const Register = () => {
   const [userInfo,setUserInfo]=useState({})
   const [registerError,setRegisterError]=useState('')
   const [success,setSuccess]=useState('')
+  const [showpassword,setShowpassword]=useState(false)
     const handleRegister=(e)=>{
          e.preventDefault();
         const email=e.target.email.value;
@@ -15,10 +16,14 @@ const Register = () => {
         setRegisterError('')
         setUserInfo({})
         setSuccess('')
-        
+
         if(password.length<6){
           setRegisterError('password more than 6 characters');
           return;
+        }
+        else if(!/[A-Z]/.test(password)){
+          setRegisterError('Should have upper case')
+          return
         }
        
         createUserWithEmailAndPassword(auth,email,password)
@@ -54,12 +59,18 @@ const Register = () => {
             </div>
             <div className="relative h-11 w-full min-w-[200px]">
               <input
-                type="password" name="password"
-                className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-              required/>
+                type={showpassword ? "text" :"password"}
+                 name="password"
+                className="peer h-full w-full rounded-md border inline border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+              required/><button onClick={()=>setShowpassword(!showpassword)}>
+                {
+                  showpassword ? <FaEyeSlash> </FaEyeSlash> :<FaEye></FaEye>
+                }
+              </button>
               <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                 Password
               </label>
+              
             </div>
           </div>
 
